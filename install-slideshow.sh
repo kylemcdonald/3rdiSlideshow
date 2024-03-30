d2c4ef73ec6b42c37296a0d33a21fd7f816e184a
Kyle McDonald <kyle@kylemcdonald.net>
2024-03-29 18:31:46 -0700

    installer

---
+++ install-slideshow.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+SERVICE_ID=slideshow
+SERVICE_NAME="slideshow"
+SCRIPT_NAME="run-slideshow.sh"
+
+# do not edit below this line
+
+USER=$(whoami)
+SERVICES_DIR=/etc/systemd/system
+SERVICE_FN="$SERVICES_DIR/$SERVICE_ID.service"
+
+echo "Installing $SERVICE_NAME to $SERVICE_FN"
+
+sudo tee "$SERVICE_FN" > /dev/null <<EOL
+[Unit]
+Description=$SERVICE_NAME
+Wants=network-online.target
+After=network-online.target
+[Service]
+WorkingDirectory=$(pwd)
+ExecStart=$(pwd)/$SCRIPT_NAME
+User=$USER
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOL
+
+sudo systemctl daemon-reload
+sudo systemctl enable $SERVICE_ID
+sudo systemctl start $SERVICE_ID
---
+++ run-slideshow.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source .venv/bin/activate
+DISPLAY=:0 /home/wafaa/Documents/3rdiSlideshow/.venv/bin/python3 /home/wafaa/Documents/3rdiSlideshow/app.py
